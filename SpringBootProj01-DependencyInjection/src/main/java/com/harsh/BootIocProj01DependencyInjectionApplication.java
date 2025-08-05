df9ebbe21332b9102ebf77f65722fa8348f4f2b6
harsh <harshwarbhe@gmail.com>
2025-08-05 14:56:38 +0530

    sb 1.1 Dependency Injection

---
+++ SpringBootProj01-DependencyInjection/src/main/java/com/harsh/BootIocProj01DependencyInjectionApplication.java
@@ -0,0 +1,50 @@
+package com.harsh;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.harsh.sbeans.SeasonFinder;
+
+@SpringBootApplication
+public class BootIocProj01DependencyInjectionApplication {
+
+	/*
+	 * note :: In spring boot Apps, we do not take separate Configuration classes
+	 * because main class having @SpringBootApplication itself act as Configuration
+	 * class
+	 */
+	
+	
+	@Bean(name = "id")
+	public LocalDate createDate() {
+		return LocalDate.now();
+
+	}
+
+	public static void main(String[] args) {
+
+		//get ioc container
+		ConfigurableApplicationContext ctx = SpringApplication.run(BootIocProj01DependencyInjectionApplication.class,args);
+		
+		//get target spring bean class obj  ref
+		SeasonFinder finder = ctx.getBean("sf", SeasonFinder.class);
+		
+		String result = finder.findSeason();
+		System.out.println(result);
+
+		System.out.println("----------------------------------------------------------------");
+		
+		String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
+		System.out.println("beans id are : "+Arrays.toString(beanDefinitionNames));
+		
+		ctx.close();
+	}
+
+}
+
+
---
+++ SpringBootProj01-DependencyInjection/src/main/java/com/harsh/SpringBootProj01DependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.harsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProj01DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProj01DependencyInjectionApplication.class, args);
+	}
+
+}
