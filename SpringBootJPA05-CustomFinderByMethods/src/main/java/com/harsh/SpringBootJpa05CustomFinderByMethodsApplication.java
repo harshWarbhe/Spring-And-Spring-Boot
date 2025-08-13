1f972f246e18645a2c52b1791c32f5588c395753
harsh <harshwarbhe@gmail.com>
2025-08-13 19:53:32 +0530

    SpringBootJPA05- CustomFinderByMethods

---
+++ SpringBootJPA05-CustomFinderByMethods/src/main/java/com/harsh/SpringBootJpa05CustomFinderByMethodsApplication.java
@@ -0,0 +1,13 @@
+package com.harsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpa05CustomFinderByMethodsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpa05CustomFinderByMethodsApplication.class, args);
+	}
+
+}
