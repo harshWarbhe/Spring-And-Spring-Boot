260e109b7df78dc9562107ac4ed382c82e2802b7
harsh <harshwarbhe@gmail.com>
2025-08-18 12:22:47 +0530

    SpringBootJPA10-InvokingMYSQL-PL-SQL_SQLProcedure-2

---
+++ SpringBootJPA10-InvokingMYSQL-PL-SQL_SQLProcedure-2/src/main/java/com/harsh/SpringBootJpa10InvokingMysqlPlSqlSqlProcedure2Application.java
@@ -0,0 +1,13 @@
+package com.harsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpa10InvokingMysqlPlSqlSqlProcedure2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpa10InvokingMysqlPlSqlSqlProcedure2Application.class, args);
+	}
+
+}
