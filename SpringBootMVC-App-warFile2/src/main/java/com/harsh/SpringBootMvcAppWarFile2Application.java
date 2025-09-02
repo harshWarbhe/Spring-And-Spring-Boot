6586a1a809fe40054a7a1fa99efa3e32d6d6ef07
harsh <harshwarbhe@gmail.com>
2025-09-02 12:01:59 +0530

    SpringBootMVC-App-warFie2

---
+++ SpringBootMVC-App-warFile2/src/main/java/com/harsh/SpringBootMvcAppWarFile2Application.java
@@ -0,0 +1,13 @@
+package com.harsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcAppWarFile2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcAppWarFile2Application.class, args);
+	}
+
+}
